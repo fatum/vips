df049f9dbf1381e9cba09e65e515cb96a87c703c
m.filippovich <m.filippovich@fun-box.ru>
2012-11-15 14:55:48 +0300

    add chrome remote debugging

--- lib/vips/thor.rb
+++ lib/vips/thor.rb
@@ -1,12 +1,47 @@
 require 'thor'
 require 'vips'
 
+require 'em-http'
+require 'faye/websocket'
+require 'json'
+
 module Vips
   class Thor < ::Thor
-    desc 'extract URL', "Set url for block's extraction"
-    def extract(url)
-      blocks = Vips::Extractor.extract_blocks(url)
-      puts blocks.inspect
+    desc 'test', "Set url for block's extraction"
+    def test
+      EM.run do
+        # Chrome runs an HTTP handler listing available tabs
+        conn = EM::HttpRequest.new('http://localhost:9222/json').get
+        conn.callback do
+          resp = JSON.parse(conn.response)
+          puts "#{resp.size} available tabs, Chrome response: \n#{resp}"
+
+          # connect to first tab via the WS debug URL
+          ws = Faye::WebSocket::Client.new(resp.first['webSocketDebuggerUrl'])
+          ws.onopen = lambda do |event|
+            # tell Chrome to navigate to twitter.com and look for "chrome" tweets
+            ws.send JSON.dump({
+              id: 2,
+              method: 'Page.navigate',
+              params: {url: 'http://lenta.ru/?' + rand(100).to_s}
+            })
+            ws.send JSON.dump({
+              id: 2,
+              method: 'DOM.highlightRect',
+              params: {
+                x: 100, y: 100,
+                width: 100, height: 100,
+                color: {r: 0, b: 0, g: 0}
+              }
+            })
+          end
+
+          ws.onmessage = lambda do |event|
+            # print event notifications from Chrome to the console
+            p [:new_message, JSON.parse(event.data)]
+          end
+        end
+      end
     end
   end
 end
